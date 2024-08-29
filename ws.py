f3da90789e1651c97cad65521db260096a55e3e3
inu1255 <929909260@qq.com>
2024-08-29 17:19:59 +0800

    ping

--- ws.py
+++ ws.py
@@ -1,37 +1,58 @@
 import websocket
 import json
-import time
 import signal
 import sys
-import argparse
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
  
 runners = {}
-ws_url = 'ws://127.0.0.1:3003/ws/deepseek?name=test'
+ws_url = os.getenv('WS_URL')
+if ws_url is None:
+    ws_url = 'ws://127.0.0.1:3003/ws/deepseek?name=test'
 
 def on_message(ws, message):
-    data = json.loads(message)
-    if data['type'] not in runners:
-        ws.send(json.dumps({'id': data['id'], 'error': f"Runner {data['type']} not found"}))
+    if message=="pong":
         return
     try:
-        ret = runners[data['type']](**data['data'])
-        print("ret", ret)
-        ws.send(json.dumps({'id': data['id'], 'data': ret}))
+        data = json.loads(message)
+        t = data['type']
+        if t not in runners:
+            ws.send(json.dumps({'id': data['id'], 'error': f"Runner {t} not found"}))
+            return
+        try:
+            print("call", t)
+            ret = runners[t](**data['data'])
+            ws.send(json.dumps({'id': data['id'], 'data': ret}))
+        except Exception as e:
+            print(e)
+            ws.send(json.dumps({'id': data['id'], 'error': str(e)}))
     except Exception as e:
         print(e)
-        ws.send(json.dumps({'id': data['id'], 'error': str(e)}))
     
 
 def on_error(ws, error):
     print(f"Error: {error}")
 
 def on_close(ws, close_status_code, close_msg):
-    if runners is None:
-        return
     print(f"Closed: {close_status_code} {close_msg}")
-    print("Reconnecting...")
-    time.sleep(2)  # Wait for 2 seconds before attempting to reconnect
-    connect_websocket()
+
+def on_open(ws):
+    pass
+
+def connect_websocket():
+    ws = websocket.WebSocketApp(ws_url,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever(
+        ping_interval=10,
+        ping_timeout=5,
+        ping_payload="ping",
+        reconnect=1
+    )
 
 def on_sigint(signum, frame):
     global runners
@@ -39,24 +60,11 @@ def on_sigint(signum, frame):
     print("Received SIGINT, closing gracefully...")
     sys.exit(0)
 
-
-def connect_websocket():
-    signal.signal(signal.SIGINT, on_sigint)
-    ws = websocket.WebSocketApp(ws_url,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.run_forever()
-
-def get_ws_url():
-    parser = argparse.ArgumentParser(description='Process some arguments.')
-    parser.add_argument('--ws', type=str, required=True, help='The websocket URL')
-    args = parser.parse_args()
-    return args.ws
-
 if __name__ == "__main__":
-    ws_url = get_ws_url()
+    signal.signal(signal.SIGINT, on_sigint)
     print(f"ws: {ws_url}")
-    from codex import codex
+    # from codex import codex
+    def codex(code="", max_length=128):
+        return code
     runners['code'] = codex
     connect_websocket()
